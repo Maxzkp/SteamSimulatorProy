833b988a130ee2dab71563c4d573221a55169253
Maxzkp <max.zkp@gmail.com>
2021-06-15 16:12:41 -0500

    Template detecction fixed

--- dirs.py
+++ dirs.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Templates')
 
 @app.route('/')
 def index():
-    return render_template('./Templates/base_template.jinja')
+    return render_template('base_template.jinja')
 
 if __name__ == '__main__':
     app.run(debug=True)
