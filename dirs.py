ffe519e2eb4d5ea04e6addb832b53efe7d185de1
Maxzkp <max.zkp@gmail.com>
2021-06-15 16:04:49 -0500

    Minor error

--- dirs.py
+++ dirs.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base_template.jinja')
+    return render_template('./Templates/base_template.jinja')
 
 if __name__ == '__main__':
     app.run(debug=True)
