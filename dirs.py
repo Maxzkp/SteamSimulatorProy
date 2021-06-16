8220c8eb2e399090f00231f633a96b147a5ca8f9
Maxzkp <max.zkp@gmail.com>
2021-06-15 22:34:13 -0500

    Massive changes to many things

--- dirs.py
+++ dirs.py
@@ -3,12 +3,35 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='Templates')
 
 @app.route('/<int:usr>')
-def index(usr):
+def main(usr):
     return render_template('main_user.html', user = usr)
+
+@app.route('/<int:usr>/profile')
+def profile(usr):
+    return render_template('user_profile.html', user = usr)
+
+#Pendiente
+@app.route('/<int:usr>/change_password')
+def change_password(usr):
+    return render_template('user_profile.html', user = usr)
+
+#Pendiente
+@app.route('/<int:usr>/get_credits')
+def get_credits(usr):
+    return render_template('user_profile.html', user = usr)
 
 @app.route('/<int:usr>/see_product/<int:prod>')
 def prod(usr, prod):
     return render_template('product.html', user = usr, prod = prod)
+
+@app.route('/<int:usr>/buy/<int:prod>')
+def buy(usr, prod):
+    return render_template('buy.html', user = usr, prod = prod)
+
+#Pendiente
+@app.route('/<int:usr>/refund/<int:prod>')
+def refund(usr, prod):
+    return render_template('buy.html', user = usr, prod = prod)
 
 if __name__ == '__main__':
     app.run(debug=True)
