b361a27d0b384eb4c43129dbe2cbd74746060602
Maxzkp <max.zkp@gmail.com>
2021-06-15 21:22:19 -0500

    Vista de producto

--- dirs.py
+++ dirs.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='Templates')
 
-@app.route('/')
-def index():
-    return render_template('main_user.html')
+@app.route('/<int:usr>')
+def index(usr):
+    return render_template('main_user.html', user = usr)
+
+@app.route('/<int:usr>/see_product/<int:prod>')
+def prod(usr, prod):
+    return render_template('product.html', user = usr, prod = prod)
 
 if __name__ == '__main__':
     app.run(debug=True)
